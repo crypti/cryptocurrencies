ae7f0fcb18ba274c2e096215c64270d41fcfe207
Brandon Him <brandonhim@live.com>
2018-01-13 13:15:22 -0800

    Update test

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ import cryptocurrencies from '.';
 
 test('produces a JSON object', t => {
 	t.is(typeof cryptocurrencies, 'object');
-	t.is(cryptocurrencies.BTC, 'Bitcoin');
+	t.is(cryptocurrencies.BTC.name, 'Bitcoin');
+	t.is(cryptocurrencies.BTC.imageUrl, 'https://raw.githubusercontent.com/crypti/cryptocurrencies/master/images/BTC.png');
+	t.is(cryptocurrencies.BTC.iconUrl, 'https://raw.githubusercontent.com/crypti/cryptocurrencies/master/images/BTC-128.png');
 });
 
 test('.symbols', t => {
