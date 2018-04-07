20f457a312f5d729e9e8ad6dc670d1164194e724
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-07 20:06:25 +0200

    Fix failing test

--- sync.js
+++ sync.js
@@ -1,46 +1,46 @@
-const fetch = require('node-fetch')
-const currentCryptocurrenciesList = require('./cryptocurrencies.json')
-const jsonfile = require('jsonfile')
-const _ = require('lodash')
+const fetch = require('node-fetch');
+const jsonfile = require('jsonfile');
+const _ = require('lodash');
+const currentCryptocurrenciesList = require('./cryptocurrencies.json');
 
 // Get all symbols from coinmarketcap (limit=0 means all)
 fetch('https://api.coinmarketcap.com/v1/ticker/?limit=0')
 .then(response => response.json())
 .then(jsonResponse => {
-	const missingCryptocurrenciesList = getMissingCurrencies(jsonResponse)
-	const missingTotal = Object.keys(missingCryptocurrenciesList).length
-	if (!missingTotal) {
-		console.log('List already seems up-to-date! We stop here.')
+	const missingCryptocurrenciesList = getMissingCurrencies(jsonResponse);
+	const missingCryptocurrenciesLength = Object.keys(missingCryptocurrenciesList).length;
+	if (missingCryptocurrenciesLength) {
+		console.log(`Found ${missingCryptocurrenciesLength} new cryptocurrencies.`);
+		const unsortedCurrenciesList = Object.assign(missingCryptocurrenciesList, currentCryptocurrenciesList);
+		const sortedCurrenciesList = sortObjectsByValue(unsortedCurrenciesList);
+		saveToFile('cryptocurrencies.json', sortedCurrenciesList);
 	} else {
-		console.log(`Found ${missingTotal} new cryptocurrencies.`)
-		const unsortedCurrenciesList = Object.assign(missingCryptocurrenciesList, currentCryptocurrenciesList)
-		const sortedCurrenciesList = sortObjectsByValue(unsortedCurrenciesList)
-		saveToFile('cryptocurrencies.json', sortedCurrenciesList)
+		console.log('List already seems up-to-date! We stop here.');
 	}
 })
-.catch(error => {
-	console.log('Error', error)
-})
+.catch(err => {
+	console.log('Error', err);
+});
 
-function saveToFile (fileName, content) {
-	jsonfile.writeFile(fileName, content, { spaces: 2 }, (error) => {
-		if (error) {
-			console.log('Shit, something went wrong. Error writing to file.', error)
+function saveToFile(fileName, content) {
+	jsonfile.writeFile(fileName, content, {spaces: 2}, err => {
+		if (err) {
+			console.log('Shit, something went wrong. Error writing to file.', err);
 		} else {
-			console.log(`Done syncing! List has now ${Object.keys(content).length} cryptocurrencies.`)
+			console.log(`Done syncing! List has now ${Object.keys(content).length} cryptocurrencies.`);
 		}
-	  })
+	});
 }
 
-function getMissingCurrencies (coinmarketcapList) {
+function getMissingCurrencies(coinmarketcapList) {
 	return coinmarketcapList.reduce((list, coinmarketcap) => {
 		if (!currentCryptocurrenciesList[coinmarketcap.symbol]) {
-			list[coinmarketcap.symbol] = coinmarketcap.name
+			list[coinmarketcap.symbol] = coinmarketcap.name;
 		}
-		return list
-	}, {})
+		return list;
+	}, {});
 }
 
 function sortObjectsByValue(unsortedObject) {
-	return _(unsortedObject).toPairs().sortBy(1).fromPairs().value()
+	return _(unsortedObject).toPairs().sortBy(1).fromPairs().value();
 }
